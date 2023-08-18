c50895e506736267ac45654ad1b8192d8b52bf8a
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-08-18 13:43:39 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+  adapter: vercel(),
   experimental: {
     viewTransitions: true,
   },
